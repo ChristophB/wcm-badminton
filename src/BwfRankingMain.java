2aac34503e36827005c787504fdfe92ae22f84bb
Marcel <m.jacob@hotmail.com>
2014-11-09 20:31:46 +0100

    Code for loading all profiles completed, but untested!

--- src/BwfRankingMain.java
+++ src/BwfRankingMain.java
@@ -1,5 +1,3 @@
-import java.net.URL;
-import java.util.ArrayList;
 
 /**
  * this class do all the work for setting up our mysql database. this includes
@@ -17,33 +15,7 @@ public class BwfRankingMain {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
-		int numberOfPages = 1;
-		int currentPage = 0;
-		WebsiteOperations mensSinglesRankingWebsite = new WebsiteOperations();
-		ArrayList<URL> linkList;
-		while (numberOfPages > 0) {
-			currentPage++;
-			String startUrl = "http://bwf.tournamentsoftware.com/ranking/category.aspx?id=7845&category=472&p="
-					+ currentPage + "&ps=100";
-			mensSinglesRankingWebsite.setWebsiteURL(startUrl);
-			mensSinglesRankingWebsite.saveWebsiteToHTMLFile();
-			if (currentPage == 1) {
-				numberOfPages = mensSinglesRankingWebsite
-						.getNumberOfPagesForDiscipline();
-			}
-			numberOfPages--;
-
-			linkList = mensSinglesRankingWebsite.createLinklistOfProfiles();
-			for (URL url : linkList) {
-				mensSinglesRankingWebsite.setWebsiteURL(url);
-				mensSinglesRankingWebsite.saveWebsiteToHTMLFile();
-				mensSinglesRankingWebsite.createCleanedFile();
-				// playerProfile.extractInformation();
-				// playerProfile.insertExtractedInformationToDatabase();
-			}
-		}
-		// mens singles completed
-
+		BwfProfileLoader loader = new BwfProfileLoader();
+		loader.loadAllProfilesAsHTML();
 	}
 }
